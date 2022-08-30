94fed0f33d960039abc4b365c950199f9613bf9d
Candan Dozen <candan.dozen@cern.ch>
2022-08-30 13:09:34 +0200

    dummytest11

--- unittests/utils/test_locate.py
+++ unittests/utils/test_locate.py
@@ -6,9 +6,12 @@ from tracker_dcs_web.utils.locate import abspath_root, abspath_data
 def test_root():
     root = abspath_root()
     elements = os.listdir(root)
+    print(elements)
     assert {"tracker_dcs_web", "unittests"}.issubset(elements)
+    print("candan==", elements)
 
 
 def test_data_path():
     dummy = abspath_data("dummy_files/test.txt")
+    print(dummy)
     assert dummy.exists()
