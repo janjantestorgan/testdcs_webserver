69937d4308c59cdb1686905e4685e58ecbe96606
Candan Dozen <candan.dozen@cern.ch>
2022-08-30 11:31:44 +0200

    test readtxt

--- unittests/utils/test_locate.py
+++ unittests/utils/test_locate.py
@@ -12,3 +12,21 @@ def test_root():
 def test_data_path():
     dummy = abspath_data("dummy_files/test.txt")
     assert dummy.exists()
+    with open("dummy_files/test.txt") as f:
+        contents = f.readlines()
+        # print(contents)
+        result = {}
+        n = {}  # pt100
+        for x in contents:
+            sublist = {}
+            n["id"] = x.split("\t")[2]
+            sublist["true_module"] = x.split("\t")[0]
+            sublist["fake_module"] = x.split("\t")[1]
+            n["modules"] = sublist  # 1. option
+            # print(n)
+            my_index = x.split("\t")[2]
+            # print(my_index)
+            result[int(my_index)] = sublist  # 2.option (turn string to int)
+
+        f.close()
+        print(result)
