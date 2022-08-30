76eb2cb57b43812db57a68a172f9747d66409075
Candan Dozen <candan.dozen@cern.ch>
2022-08-30 13:54:31 +0200

    read txt file in unittest

--- unittests/utils/test_locate.py
+++ unittests/utils/test_locate.py
@@ -6,12 +6,32 @@ from tracker_dcs_web.utils.locate import abspath_root, abspath_data
 def test_root():
     root = abspath_root()
     elements = os.listdir(root)
-    print(elements)
     assert {"tracker_dcs_web", "unittests"}.issubset(elements)
-    print("candan==", elements)
 
 
 def test_data_path():
     dummy = abspath_data("dummy_files/test.txt")
-    print(dummy)
+    with open (dummy) as f:
+        content = f.readlines()
+        result = {}
+        #n = {}  # pt100_id
+        for x in content:
+            sublist = {}
+            #n["id"] = x.split("\t")[2]
+            sublist["true_module"] = x.split("\t")[0]
+            sublist["fake_module"] = x.split("\t")[1]
+            #n["modules"] = sublist  # 1. option
+            #print(n)
+            my_index = x.split("\t")[2]
+            result[int(my_index)] = sublist  # 2.option (turn string to int)
+
+        f.close()
+        print(result)
     assert dummy.exists()
+
+
+
+
+
+
+         
